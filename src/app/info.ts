2a7df3a5ea176919597da0787d43e4607fceb268
Wael Hasan <aboeyas87@gmail.com>
2023-11-25 21:26:11 +0200

    Moved data to a dedicated file

---
+++ src/app/info.ts
@@ -0,0 +1,19 @@
+export default {
+  fullName: "Wael Hasan",
+  nationality: "Egypian",
+  title: "Senior Full-Stack JS/TS web developer",
+  technicalSkills: ['HTML5', 'CSS3', 'Javascript', 'Typescript', 'Nodejs', 'Expressjs', 'RESTful API design', 'Graphql', 'Reactjs', 'Redux', 'Nextjs', 'Jest', 'Cypress', 'Storybook', 'SQL', 'Sequelize ORM', 'Mongodb', 'Mongoose ODM', 'Contentful', 'Git', 'Github', 'Linux', 'Bash', 'Design patterns', 'Gulp', 'Socket.io', 'Deepstream', 'Redis', 'Docker', 'AWS', 'TypeORM', 'JSON', 'JSON schema', 'Mocha', 'Chai', 'Enzyme', 'SASS', 'Yeoman'],
+  softSkills: ['Teamwork', 'Leadership', 'Humility', 'Problem-Solving', 'Self-starter', 'Pragmatic', 'Communication', 'Collaboration', 'Mentorship', 'Lifelong learner'],
+  reasonsToHireMe: [
+    "Passion for being a better software engineer",
+    "Passion for learning more about technology in general and JS in particular",
+    "Fast learner",
+    "Passion for building well engineered useful stuff and see people use it happily",
+    "Good team player",
+    "A lazy developer that searches for the simplest best way to make the code as reliable, self-documenting, and maintainable as possible; so it takes less effort for any change from me or my colleagues"
+  ],
+  reasonsNotToHireMe: [
+    "To do repetitive work, that does not leverage my technical skills",
+    "If you work on an industry that contradicts with my faith and believs, or I prefer to stay away from, which are: porn, weaponary, racism, and insurance."
+  ]
+}

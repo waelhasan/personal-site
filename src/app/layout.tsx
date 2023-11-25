f6d87b1cf4870b269686707453a018999327c650
Wael Hasan <aboeyas87@gmail.com>
2023-11-25 19:42:46 +0200

    Remove unneeded google font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,6 @@
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
 import './globals.css'
 import Navbar from './components/Navbar'
-
-const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Wael Hasan',
@@ -16,19 +13,20 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="ar" style={{
-      height: "100%",
-      width: "100vw",
-      overflowX: "hidden",
-      fontFamily: "sans-serif",
-      color: "var(--foreground-rgb)",
-      background: `linear-gradient(
+    <html
+      lang="ar"
+      style={{
+        height: "100%",
+        width: "100vw",
+        overflowX: "hidden",
+        fontFamily: "sans-serif",
+        color: "var(--foreground-rgb)",
+        background: `linear-gradient(
                       to bottom,
                       transparent,
                       var(--background-end-rgb)) var(--background-start-rgb)`
-    }}>
+      }}>
       <body
-        className={inter.className}
         style={{
           height: "100%",
           width: "100%",

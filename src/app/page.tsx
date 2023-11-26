bc69a509a0c59bc2864cdc4bf9cd77100cf92bc7
Wael Hasan <aboeyas87@gmail.com>
2023-11-26 19:41:10 +0200

    Added UnderconstructionNotice component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -268,6 +268,25 @@ const Experiences = () => (
   </section>
 )
 
+const UnderconstructionNotice = () => (
+  <h1 style={{
+    fontSize: "2rem",
+    textAlign: "center",
+    color: "yellow",
+    border: "5px dashed yellow",
+    backgroundColor: "indianred",
+    padding: "1rem",
+    alignSelf: "flex-start",
+    transform: "translate(-2%, 30%) rotateZ(-45deg)"
+  }}>
+    <span style={{ fontSize: "3rem"}}>
+      ⚠️
+    </span>
+    <br />
+    Under construction
+  </h1>
+)
+
 export default function Home() {
   return (
     <main style={{
@@ -276,6 +295,7 @@ export default function Home() {
       justifyContent: "center",
       alignItems: "center"
     }}>
+      <UnderconstructionNotice />
       <Intorduction />
       <ReasonsToHireMeOrNotToHireMe />
       <Skills />

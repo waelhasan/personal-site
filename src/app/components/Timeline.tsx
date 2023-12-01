791da82f5c781d49a24b8be4789add5093663a7e
Wael Hasan <aboeyas87@gmail.com>
2023-12-01 16:18:51 +0200

    Responsive Timeline using Tailwind

--- src/app/components/Timeline.tsx
+++ src/app/components/Timeline.tsx
@@ -1,6 +1,56 @@
 import { Important } from "./Important"
 import { Skill } from "./Skills"
 
+const RightArticle = ({ key, children }: { key: string, children: React.ReactNode }) => (
+    <>
+        {/* The line that fills the gap between individual boxes and the timeline */}
+        <span className={`
+            w-[1rem] 
+            border-[1px] border-solid border-[--foreground-section-title-rgb] 
+            translate-y-[1.11rem] translate-x-[1rem] xl:translate-x-[2rem]
+        `} />
+        {/* The box that shows element details */}
+        <article key={key} className={`
+            w-[36rem]
+            text-[1.4rem]
+            my-[1rem] mx-0
+            p-[1rem]
+            border-[1px] border-solid border-[--foreground-section-title-rgb] rounded-b-[40px]
+            bg-[--darker-bg-rgb]
+            
+            rounded-tl-[40px] xl:rounded-tl-none xl:rounded-tr-[40px]
+            xl:translate-x-[calc(100%+2rem)]
+        `}>
+            {children}
+        </article>
+    </>
+)
+
+const LeftArticle = ({ key, children }: { key: string, children: React.ReactNode }) => (
+    <>
+        {/* The line that fills the gap between individual boxes and the timeline */}
+        <span className={`
+            w-[1rem] 
+            border-[1px] border-solid border-[--foreground-section-title-rgb] 
+            translate-x-[1rem] translate-y-[1.11rem]
+        `} />
+        {/* The box that shows element details */}
+        <article key={key} className={`
+            w-[36rem]
+            text-[1.4rem]
+            my-[1rem] mx-0
+            p-[1rem]
+            border-[1px] border-solid border-[--foreground-section-title-rgb] rounded-b-[40px]
+            bg-[--darker-bg-rgb]
+            
+            xl:text-right
+            rounded-tl-[40px]
+        `}>
+            {children}
+        </article>
+    </>
+)
+
 interface TimelineElement {
     title: string
     company: {
@@ -13,10 +63,49 @@ interface TimelineElement {
     skills: string[]
 }
 
+const Article = ({ isOdd, experience }: { isOdd: boolean, experience: TimelineElement }) => {
+    const ParentArticle = isOdd ? LeftArticle : RightArticle
+    const formatter = new Intl.DateTimeFormat('en', { month: 'short', year: "numeric" })
+    const fromDate = formatter.format(new Date(experience.from))
+    const toDate = formatter.format(new Date(experience.to))
+    const fromTo = `${fromDate} - ${toDate}`
+
+    return (
+        <>
+            {/* The circle in timeline */}
+            <span className={`
+                translate-x-[1.5rem] translate-y-[1.7rem]
+                w-[1rem] h-[1rem]
+                bg-[--foreground-section-title-rgb]
+                border-[1px] border-solid rounded-[50%]
+            `} />
+            <ParentArticle key={fromTo}>
+                <div style={{ fontFamily: "monospace" }}>
+                    {fromTo}
+                </div>
+                <Important level={5} color="var(--darker-foreground-golden-rgb)">
+                    {experience.title}
+                </Important>
+                <br />
+                <Important level={6}>
+                    {experience.company.name + ` (${experience.type[0] + experience.type.slice(1).toLocaleLowerCase()})`}
+                </Important>
+                <details>
+                    <summary>
+                        Read more
+                    </summary>
+                    <h2 style={{ textAlign: "justify" }}>{experience.summary}</h2>
+                    {experience.skills.map(skill => <Skill key={skill} title={skill} />)}
+                </details>
+            </ParentArticle>
+        </>
+    )
+}
+
 const Timeline = ({ id, title, elements }: { id: string, title: string, elements: TimelineElement[] }) => (
-    <section id={id} className="-translate-x-1/2">
+    <section id={id} className="xl:-translate-x-1/2">
         <h1 className={`
-            absolute left-[100%] -translate-x-1/2
+            absolute left-[50%] xl:left-[100%] -translate-x-1/2
             rounded-[50%] border-b-[1px] border-b-solid border-b-[foreground-section-title-rgb]
             w-max
             p-[1rem]
@@ -29,67 +118,7 @@ const Timeline = ({ id, title, elements }: { id: string, title: string, elements
                 p-[1rem] 
                 border-r-[1px] border-r-solid border-r-[--foreground-section-title-rgb]
             `}>
-                {elements.map((experience, index) => {
-                    const formatter = new Intl.DateTimeFormat('en', { month: 'short', year: "numeric" });
-                    const isOdd = index % 2 === 0
-                    const fromDate = formatter.format(new Date(experience.from))
-                    const toDate = formatter.format(new Date(experience.to))
-
-                    const fromTo = `${fromDate} - ${toDate}`
-                    return (
-                        <>
-                            {/* The circle in timeline */}
-                            <span style={{
-                                width: "1rem",
-                                height: "1rem",
-                                backgroundColor: "var(--foreground-section-title-rgb)",
-                                border: "1px solid var(--foreground-section-title-rgb)",
-                                borderRadius: "50%",
-                                transform: `translate(1.5rem, 1.5rem)`
-                            }} />
-                            {/* The line that fills the gap between individual boxes and the timeline */}
-                            <span style={{
-                                width: "1rem",
-                                borderTop: "1px solid var(--foreground-section-title-rgb)",
-                                transform: `translate(${isOdd ? 2 : 1}rem, 1.06rem)`
-                            }} />
-                            {/* The box that shows element details */}
-                            <article
-                                key={fromTo}
-                                style={{
-                                    textAlign: isOdd ? "left" : "right",
-                                    width: "36rem",
-                                    fontSize: "1.4rem",
-                                    margin: "1rem 0",
-                                    padding: "1rem",
-                                    border: "1px solid var(--foreground-section-title-rgb)",
-                                    borderBottomRightRadius: "40px",
-                                    [isOdd ? "borderTopRightRadius" : "borderTopLeftRadius"]: "40px",
-                                    borderBottomLeftRadius: "40px",
-                                    ...(isOdd ? { transform: "translateX(calc(100% + 2rem))" } : {}),
-                                    backgroundColor: "var(--darker-bg-rgb)"
-                                }}>
-                                <div style={{ fontFamily: "monospace" }}>
-                                    {fromTo}
-                                </div>
-                                <Important level={5} color="var(--darker-foreground-golden-rgb)">
-                                    {experience.title}
-                                </Important>
-                                <br />
-                                <Important level={6}>
-                                    {experience.company.name + ` (${experience.type[0] + experience.type.slice(1).toLocaleLowerCase()})`}
-                                </Important>
-                                <details>
-                                    <summary style={{ transition: "all 0.5s" }}>
-                                        Read more
-                                    </summary>
-                                    <h2 style={{ textAlign: "justify" }}>{experience.summary}</h2>
-                                    {experience.skills.map(skill => <Skill key={skill} title={skill} />)}
-                                </details>
-                            </article>
-                        </>
-                    )
-                })}
+                {elements.map((experience, index) => <Article isOdd={index % 2 === 0} experience={experience} />)}
             </div>
         </div>
     </section>

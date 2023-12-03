f84efc1a4aabe0203db96c31f2d196d596b960e8
Wael Hasan <aboeyas87@gmail.com>
2023-12-03 10:32:29 +0200

    Fixed accounts icon links position in smaller screens

--- src/app/components/MyAccountsLinks.tsx
+++ src/app/components/MyAccountsLinks.tsx
@@ -9,14 +9,22 @@ export interface IMyAccountsLinksProps {
 }
 
 export const MyAccountsLinks = ({ linkedinUrl, githubUrl, npmUrl, bloggerUrl }: IMyAccountsLinksProps) => (
-    <div style={{
-        display: "inline-flex",
-        gap: "1rem",
-        justifyContent: "center",
-        fontSize: "4rem",
-        color: "var(--foreground-rgb-important)",
-        verticalAlign: "middle"
-    }}>
+    <div className={`
+            inline-flex
+            gap-[0.5rem] sm:gap-[1rem]
+            justify-center
+            text-[2rem] sm:text-[4rem] text-[--foreground-rgb-important]
+            align-middle
+        `}
+    // style={{
+    //     display: "inline-flex",
+    //     gap: "1rem",
+    //     justifyContent: "center",
+    //     fontSize: "4rem",
+    //     color: "var(--foreground-rgb-important)",
+    //     verticalAlign: "middle"
+    // }}
+    >
         <IconLink url={linkedinUrl} Icon={FaLinkedin} />
         <IconLink url={githubUrl} Icon={FaGithub} />
         <IconLink url={npmUrl} Icon={FaNpm} />

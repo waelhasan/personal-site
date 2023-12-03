402f21b9a97fcdfea8ed05f69d6184a6cbe6decd
Wael Hasan <aboeyas87@gmail.com>
2023-12-03 10:35:43 +0200

    Cleanup

--- src/app/components/MyAccountsLinks.tsx
+++ src/app/components/MyAccountsLinks.tsx
@@ -10,21 +10,12 @@ export interface IMyAccountsLinksProps {
 
 export const MyAccountsLinks = ({ linkedinUrl, githubUrl, npmUrl, bloggerUrl }: IMyAccountsLinksProps) => (
     <div className={`
-            inline-flex
-            gap-[0.5rem] sm:gap-[1rem]
-            justify-center
-            text-[2rem] sm:text-[4rem] text-[--foreground-rgb-important]
-            align-middle
-        `}
-    // style={{
-    //     display: "inline-flex",
-    //     gap: "1rem",
-    //     justifyContent: "center",
-    //     fontSize: "4rem",
-    //     color: "var(--foreground-rgb-important)",
-    //     verticalAlign: "middle"
-    // }}
-    >
+        inline-flex
+        gap-[0.5rem] sm:gap-[1rem]
+        justify-center
+        text-[2rem] sm:text-[4rem] text-[--foreground-rgb-important]
+        align-middle
+    `}>
         <IconLink url={linkedinUrl} Icon={FaLinkedin} />
         <IconLink url={githubUrl} Icon={FaGithub} />
         <IconLink url={npmUrl} Icon={FaNpm} />

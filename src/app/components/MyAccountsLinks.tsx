aac6df0f7c96af61b0ca6a04ce02fa779d1eee32
Wael Hasan <aboeyas87@gmail.com>
2023-12-06 21:15:33 +0200

    Sticky accounts links

--- src/app/components/MyAccountsLinks.tsx
+++ src/app/components/MyAccountsLinks.tsx
@@ -1,5 +1,9 @@
+"use client"
+
+import { ReactNode, useRef } from "react"
 import { FaGithub, FaLinkedin, FaBlogger, FaNpm } from "react-icons/fa"
 import IconLink from "./IconLink"
+import useIsVisible from "./useIsVisible"
 
 export interface IMyAccountsLinksProps {
     linkedinUrl?: string
@@ -8,20 +12,52 @@ export interface IMyAccountsLinksProps {
     bloggerUrl?: string
 }
 
+const HorizontalLinks = ({ children }: { children: ReactNode }) => (
+    <div className="
+        inline-flex
+        gap-[0.5rem] sm:gap-[1rem]
+        
+    ">
+        {children}
+    </div>
+)
+
+const VerticalLinks = ({ children }: { children: ReactNode }) => (
+    <div className="
+        inline-flex flex-col
+        fixed top-[50%] left-0
+        translate-y-[-50%]
+        z-1000
+        gap-[0.5rem] sm:gap-[1rem]
+        bg-[--darker-bg-rgb]
+        p-[1rem]
+    ">
+        {children}
+    </div>
+)
+
 export const MyAccountsLinks = ({
     linkedinUrl,
     githubUrl,
     npmUrl,
     bloggerUrl
-}: IMyAccountsLinksProps) => (
-    <div className="
-        inline-flex justify-center items-center
-        gap-[0.5rem] sm:gap-[1rem]
-        text-[2rem] sm:text-[3rem] text-[--foreground-rgb-important]
-    ">
-        <IconLink url={linkedinUrl} Icon={FaLinkedin} />
-        <IconLink url={githubUrl} Icon={FaGithub} />
-        <IconLink url={npmUrl} Icon={FaNpm} />
-        <IconLink url={bloggerUrl} Icon={FaBlogger} />
-    </div>
-)
+}: IMyAccountsLinksProps) => {
+    const parentRef = useRef(null)
+    const isParentVisible = useIsVisible(parentRef)
+    const LinksContainer = isParentVisible ? HorizontalLinks : VerticalLinks
+
+    return (
+        <div ref={parentRef}
+            className="
+            inline-flex items-center
+            text-[2rem] sm:text-[3rem] text-[--foreground-rgb-important]
+        ">
+            <LinksContainer>
+                <IconLink url={linkedinUrl} Icon={FaLinkedin} />
+                <IconLink url={githubUrl} Icon={FaGithub} />
+                <IconLink url={npmUrl} Icon={FaNpm} />
+                <IconLink url={bloggerUrl} Icon={FaBlogger} />
+            </LinksContainer>
+        </div>
+    )
+}

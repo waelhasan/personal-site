92bacb25d8003da86f0587ad5a3ce42320d17fb8
Wael Hasan <aboeyas87@gmail.com>
2023-12-10 17:58:23 +0200

    Better Layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -55,12 +55,19 @@ export default function RootLayout({
           overflow-x-hidden
           scroll-smooth
         `}>
-        <UnderconstructionNotice />
         <Navbar />
-        <FreedomSupportNotice />
-        {children}
-        <ScrollToTopButton />
-        <Footer />
+        <div className="
+          flex flex-col justify-between
+          h-[calc(100%-5rem)] w-full
+        ">
+          <div>
+            <UnderconstructionNotice />
+            <FreedomSupportNotice />
+          </div>
+          {children}
+          <ScrollToTopButton />
+          <Footer />
+        </div>
         <SpeedInsights />
         <Analytics />
       </body>
